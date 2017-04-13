4fc73a782dcdc1e17851342e18076096ba8f5348
Paul Duca <paul.r.duca@gmail.com>
2017-04-13 17:44:08 -0400

    sign up /login buttons

--- App/config/routes.rb
+++ App/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :welcome, only: [:index]
+  resources :blog_posts, only: [:index, :create, :destroy]
+  root 'welcome#index'
 end
--- App/db/schema.rb
+++ App/db/schema.rb
@@ -10,17 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410183458) do
+ActiveRecord::Schema.define(version: 20170410210530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "blog_posts", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "content"
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password_hash", null: false
-    t.string   "email",         null: false
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "password_digest", null: false
+    t.string   "email",           null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
